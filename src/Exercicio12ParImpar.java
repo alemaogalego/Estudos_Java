539567e5e9db09d7e6e41417361b3893542a1c7e
Lucas César Lorena <lucascesar.lorena@gmail.com>
2026-02-13 13:37:48 -0300

    Exercicio 12 concluido

--- src/Exercicio12ParImpar.java
+++ src/Exercicio12ParImpar.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Exercicio12ParImpar {
+    static void main() {
+        Scanner leitor = new Scanner(System.in);
+        System.out.print("Digite um número inteiro: ");
+        int numero = leitor.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é par.");
+        } else {
+            System.out.println("O número " + numero + " é ímpar.");
+        }
+    }
 }
